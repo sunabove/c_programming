9591d2a625409f5fc10bd5f99422c840f259aadb
sunabove <sunabove@nate.com>
2021-11-27 00:18:27 -0800

    **

--- array/array_2d.c
+++ array/array_2d.c
@@ -2,9 +2,19 @@
 #include <stdio.h>
 
 void main() {
-  int arr[8][9];
+  int a[8][9];
   
   for(int i = 0; i < sizeof(a)/sizeof(a[0]) ; i++ ) {
+    for(int k = 0; k < sizeof(a[i])/sizeof(a[i][0]); k++ ) {
+        a[i][k] = (i + 2)*(k+ 1);
+    }
+  }
+  
+  for(int i = 0; i < sizeof(a)/sizeof(a[0]) ; i++ ) {
+    for(int k = 0; k < sizeof(a[i])/sizeof(a[i][0]) ; k ++ ) {
+        printf( "a[%d][%d] = %d \n", i, k, a[i][k] );
+    }
+    printf( "\n" );
   }
      
 }
