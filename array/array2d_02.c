4a3abcd1539ef79eba35d22c5ac4b4d22fa34000
sunabove <sunabove@nate.com>
2021-11-27 00:33:01 -0800

    **

--- array/array2d_02.c
+++ array/array2d_02.c
@@ -1,14 +1,8 @@
-// File Name : array1.c
+// File Name : array2d_02.c
 #include <stdio.h>
 
 void main() {
-  int a[8][9];
-  
-  for(int i = 0; i < sizeof(a)/sizeof(a[0]) ; i++ ) {
-    for(int k = 0; k < sizeof(a[i])/sizeof(a[i][0]); k++ ) {
-        a[i][k] = (i + 2)*(k+ 1);
-    }
-  }
+  int a[8][9] = { { 1, 2, 3, 4, 5, 6, 7, 8, 9 } };
   
   for(int i = 0; i < sizeof(a)/sizeof(a[0]) ; i++ ) {
     for(int k = 0; k < sizeof(a[i])/sizeof(a[i][0]) ; k ++ ) {
